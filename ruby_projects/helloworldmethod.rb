7a7ee1bc2e6f2a4f3f58a198fe31359ecf31836a
Rails Project by mayuran <bcamayu26@gmail.com>
2017-06-19 19:29:51 +0000

    create rails application and setup home and about pages

---
+++ ruby_projects/helloworldmethod.rb
@@ -0,0 +1,14 @@
+def hello
+    puts "Hello world!"
+    
+end
+
+hello
+
+
+def parameter(values)
+puts values    
+end
+
+parameter"my name is mayuran";
+parameter"you can call me multiple times";
---
+++ ruby_projects/playground.rb
@@ -0,0 +1,5 @@
+puts "Enter your first name"
+
+first_name=gets.chomp;
+
+puts "welcome #{first_name} to the playground!"
